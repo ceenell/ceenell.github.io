dc2b092d988631bfefb64f8b6da952c13c7c1d6d
collnell <collnellphd@gmail.com>
2020-06-30 22:05:01 -0500

    small changes

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function random_bg_color() {
     var x2 = Math.floor(Math.random() * 256);
     var y2 = Math.floor(Math.random() * 256);
     var z2 = Math.floor(Math.random() * 256);
-    var bgColor2 = "rgb(" + x2 + "," + y2 + "," + z2 + ")";
+    var bgColor2 = "rgb(" + x2 + "," + y2 + "," + y2 + ")";
 
     var grad = Math.floor(Math.random() * 360);
 
@@ -15,7 +15,7 @@ function random_bg_color() {
  console.log(bgColor2); 
   
     document.body.style.background = bgColor;
-    document.body.style.backgroundImage = "linear-gradient("+ grad +"deg, "+ bgColor +", "+ bgColor2 +")";
+    document.body.style.backgroundImage = "linear-gradient("+ grad +"deg, "+ bgColor +", "+ bgColor +"45%, "+ bgColor2 +")";
     }
 
 random_bg_color();
